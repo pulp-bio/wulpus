599824b01116459034ce64d2ab1b28d54a84acc5
Cédric Hirschi <cedrichirschi.21@gmail.com>
2023-12-11 15:05:05 +0100

    Add contribution names/headers

--- sw/wulpus/uss_conf.py
+++ sw/wulpus/uss_conf.py
@@ -1,6 +1,7 @@
 """
    Copyright (C) 2023 ETH Zurich. All rights reserved.
    Author: Sergei Vostrikov, ETH Zurich
+           Cedric Hirschi, ETH Zurich
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
